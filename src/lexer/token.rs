b795861488086e1202b33083aeeb51abe54b23b8
nongtajkrub <sunanborthwic@gmail.com>
2025-04-14 23:36:14 +0700

    Add Token struct

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -14,3 +14,25 @@ pub enum TokenType {
     /// key: value`,`
     Separator,
 }
+
+pub struct Token {
+    lexeme: String,
+    token_type: TokenType,
+}
+
+impl Token {
+    pub fn new(lexeme: &str, token_type: TokenType) -> Self {
+        Token {
+            lexeme: String::from(lexeme),
+            token_type
+        }
+    }
+
+    pub fn lexeme(&self) -> &str {
+        &self.lexeme
+    }
+    
+    pub fn token_type(&self) -> TokenType {
+        self.token_type
+    }
+}
