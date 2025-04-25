8fa3ec52b40dc9055ae13906ca4d86b51d50ed45
nongtajkrub <sunanborthwic@gmail.com>
2025-04-25 12:35:37 +0700

    impl partial eq for JsonError.

--- src/error.rs
+++ src/error.rs
@@ -8,8 +8,30 @@ pub enum JsonError {
     #[error("Invalid path to value.")]
     InvalidPath,
 
+    #[error("Invalid Json")]
+    InvalidJson,
+
+    #[error("Key you are attempting to insert already exsit use change instead.")]
+    InsertKeyAlreadyExsist,
+
     #[error("Std Input Output Error: {0}")]
     StdInputOutputError(#[from] std::io::Error),
+}
+
+impl PartialEq for JsonError {
+    fn eq(&self, other: &Self) -> bool {
+        use JsonError::*;
+
+        match (self, other) {
+            (NoPathProvided, NoPathProvided) => true,
+            (InvalidPath, InvalidPath) => true,
+            (InvalidJson, InvalidJson) => true,
+            (InsertKeyAlreadyExsist, InsertKeyAlreadyExsist) => true,
+            (StdInputOutputError(_), StdInputOutputError(_)) => true,
+
+            _ => false,
+        }
+    }
 }
 
 pub type JsonResult<T> = Result<T, JsonError>;
